16b8384a8e1773ba9be57a70e61893c0f13abdfa
Yixiong Fang <6135847+hayyyyyyden@users.noreply.github.com>
2020-04-23 23:01:08 +1000

    Finish DataHandler base class.

--- data.py
+++ data.py
@@ -5,3 +5,34 @@ import pandas as pd
 from abc import ABCMeta, abstractmethod
 
 from event import MarketEvent
+
+class DataHandler(object):
+    """
+    DataHandler is an abstract base class providing an interface for
+    all subsequent (inherited) data handlers (both live and historic).
+
+    The goal of a (derived) DataHandler object is to output a generated
+    set of bars (OLHCVI) for each symbol requested.
+
+    This will replicate how a live strategy would function as current
+    market data would be sent "down the pipe". Thus a historic and live
+    system will be treated identically by the rest of the backtesting suite.
+    """
+
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def get_latest_bars(self, symbol, N=1):
+        """
+        Returns the last N bars from the latest_symbol list,
+        or fewer if less bars are available.
+        """
+        raise NotImplementedError("Should implement get_latest_bars()")
+
+    @abstractmethod
+    def update_bars(self):
+        """
+        Pushes the latest bar to the latest symbol structure
+        for all symbols in the symbol list.
+        """
+        raise NotImplementedError("Should implement update_bars()")
