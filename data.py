65e3528ad1732be12b09cd56e121a697be8ad2f9
Yixiong Fang <6135847+hayyyyyyden@users.noreply.github.com>
2020-04-23 23:02:49 +1000

    Finish HistoricCSVDataHandler to read csv locally. 完成读写csv文件的数据读取器。
    https://www.quantstart.com/articles/Event-Driven-Backtesting-with-Python-Part-III/

--- data.py
+++ data.py
@@ -36,3 +36,35 @@ class DataHandler(object):
         for all symbols in the symbol list.
         """
         raise NotImplementedError("Should implement update_bars()")
+
+
+class HistoricCSVDataHandler(DataHandler):
+    """
+    HistoricCSVDataHandler is designed to read CSV files for
+    each requested symbol from disk and provide an interface
+    to obtain the "latest" bar in a manner identical to a live
+    trading interface.
+    """
+
+    def __init__(self, events, csv_dir, symbol_list):
+        """
+        Initialises the historic data handler by requesting
+        the location of the CSV files and a list of symbols.
+
+        It will be assumed that all files are of the form
+        'symbol.csv', where symbol is a string in the list.
+
+        Parameters:
+        events - The Event Queue.
+        csv_dir - Absolute directory path to the CSV files.
+        symbol_list - A list of symbol strings.
+        """
+        self.events = events
+        self.csv_dir = csv_dir
+        self.symbol_list = symbol_list
+
+        self.symbol_data = {}
+        self.latest_symbol_data = {}
+        self.continue_backtest = True
+
+        self._open_convert_csv_files()
